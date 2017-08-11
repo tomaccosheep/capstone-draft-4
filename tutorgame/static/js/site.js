6010110a0eb00b9126f0b8287d00323d330f3491
Al Burns <al.burns.email@gmail.com>
2017-08-11 14:23:57 -0700

    redesigned site

--- tutorgame/static/js/site.js
+++ tutorgame/static/js/site.js
@@ -27,32 +27,34 @@ $.ajaxSetup({
     }
 });
 
-
-
-
-
-
-
-
-
 /*------------------------Declare Variables------------------*/
 
 // Declares the size of various stats: the four main buttons, costs for
 // those buttons, and an update counter
 // {{
-// 4 buttons
-var points = 0;
-var click_size = 1;
-var machines = 0;
-var speed = 100;
+// 3 stats
+var well_being = 0;
+var money = 0;
+var popularity = 0;
 
-// 3 cost
-var size_cost = 1;
-var speed_cost = 1;
-var machine_cost = 1;
+// things owned
+var veg = 1;
+var pizza = 1;
+var pizzar = 0
+var shoe = 0;
+var partner = 0;
 
-// machines that collect automatically
-var reserved_machines = 0;
+// relative cost
+
+var veg_cost_money = 1;
+var pizza_cost_money = 10;
+var pizzar_cost_money = 10000;
+var shoes_cost_money = 100;
+var partner_cost_money = 500;
+var partner_cost_pop = 50;
+
+// cs demand collects automatically
+var cs_demand = 0;
 
 // counter
 var update_count = 0;
@@ -70,30 +72,138 @@ machines" html, and then restart the counter. */
 // {{
 function update() {
     update_count += 1;
-    points += machines * .002 * (speed / 100);
+    well_being += popularity * .002;
     if (update_count >= 20) {
-        reserved_machines += 1;
-        $("#collecting").html("Reserved machines: " + reserved_machines);
+        cs_demand += 1 + popularity;
+        $("#collecting").html("CS Tutor Demand: " + Math.round(cs_demand));
         update_count = 0;
     }
-    $("#stat2").html((Math.round(points * 100)/100));
+    $("#wellnum").html((Math.round(well_being * 100)/100));
     
 }
 // }}
 
 
-// Adds click_size to total points, and updates html
-// {{
-$("#pic_point").click(function() {
-    points += click_size;
-    $("#stat2").html((Math.round(points * 100)/100));
-});
-// }}
+/*----------------------HOVER-------------------------*/
 
-// These three buttons all do three things each: if you can afford
-// the cost, it adds the bought stat, it subtracts the cost from
-// points, and it increases the price. HTML is updated after each
-// {{
+$("#wellstat").hover(function() {
+    $("#infodiv").html("This is your well-being. You should try to have more well-being.");
+    }, function(){ $("#infodiv").html(" ");
+});    
+
+$("#moneystat").hover(function() {
+    $("#infodiv").html("This is how much money you have.");
+    }, function(){ $("#infodiv").html(" ");
+});    
+
+$("#popstat").hover(function() {
+    $("#infodiv").html("This is how popular you are. Being popular helps you have a sense of well-being.");
+    }, function(){ $("#infodiv").html(" ");
+});    
+
+
+$("#clickobj").hover(function() {
+    $("#infodiv").html("You will receive $10 for every night you babysit. You can babysit an unlimited amount.")
+    }, function(){ $("#infodiv").html(" ");
+});    
+
+
+$("#vegobj").hover(function() {
+    $("#infodiv").html("Does eating vegetables increase your well-being?<br />Cost: $" + veg_cost_money);
+    }, function(){ $("#infodiv").html(" ");
+});    
+
+
+$("#pizzaobj").hover(function() {
+    $("#infodiv").html("Does eating pizza increase your well-being?<br />Cost: $" + pizza_cost_money);
+    }, function(){ $("#infodiv").html(" ");
+});    
+
+$("#pizzarobj").hover(function() {
+    $("#infodiv").html("Your very own pizza restaurant! It's sure to bring money and popularity!<br />Cost: $" + pizzar_cost_money);
+    }, function(){ $("#infodiv").html(" ");
+});    
+
+$("#shoeobj").hover(function() {
+    $("#infodiv").html("Cool shoes immediately make people more popular!<br />Cost: $" + shoes_cost_money);
+    }, function(){ $("#infodiv").html(" ");
+});    
+
+
+$("#dateobj").hover(function() {
+    $("#infodiv").html("Increases your popularity, and gives a short-term boost to well-being. Dating is more effective if you're already popular.<br />Cost: $" + partner_cost_money + " and " + partner_cost_pop + " popularity.");
+    }, function(){ $("#infodiv").html(" ");
+});    
+
+
+$("#collecting").hover(function() {
+    $("#infodiv").html("This is how much demand there is for your CS tutoring skills. Demand goes up when you are more popular.");
+    }, function(){ $("#infodiv").html(" ");
+});    
+
+
+$("#study20").hover(function() {
+    $("#infodiv").html("Help someone study, and you will receive $" + Math.round(cs_demand * .2));
+    }, function(){ $("#infodiv").html(" ");
+});    
+
+
+$("#test80").hover(function() {
+    $("#infodiv").html("Take a test for a lazy CS student, and you will receive $" + Math.round(cs_demand * .8));
+    }, function(){ $("#infodiv").html(" ");
+});    
+
+
+$("#save").hover(function() {
+    $("#infodiv").html("Click this to save the game.<br />Important: Make sure you bookmark this URL, because this URL will redirect you to your saved game. Without this URL, you will lose your game.")
+    }, function(){ $("#infodiv").html(" ");
+});    
+
+
+/*-------------------------CLICK---------------------------*/
+
+$("#clickobj").click(function() {
+    money += 10;
+    $("#moneynum").html((Math.round(money * 100)/100));
+}); 
+
+$("#vegobj").click(function() {
+    veg += 1;
+    $("#vegobj").html("Buy Vegetable (" + Math.round(veg - 1) + ")");
+    well_being += (veg/(.95*veg))*pizza;
+    $("#wellnum").html((Math.round(well_being * 100)/100));
+}); 
+
+$("#pizzaobj").click(function() {
+    pizza += 1;
+    $("#pizzaobj").html("Buy Pizza (" + Math.round(pizza - 1) + ")");
+    well_being += ((veg/(.99*veg))*pizza);
+    console.log(well_being);
+    $("#wellnum").html((Math.round(well_being * 100)/100));
+}); 
+
+$("#pizzarobj").click(function() {
+    pizzar += 1;
+    $("#pizzarobj").html("Buy Pizza Restaurant (" + Math.round(pizzar) + ")");
+}); 
+
+$("#shoeobj").click(function() {
+    shoe += 2;
+    $("#shoeobj").html("Buy Cool Shoes (" + Math.round(shoe) + ")");
+    popularity += 10;
+    $("#popnum").html(popularity);
+}); 
+
+
+$("#dateobj").click(function() {
+    partner += 1;
+    $("#dateobj").html("Get Dating Partner (" + Math.round(partner) + ")");
+}); 
+
+
+
+
+/*
 $("#pic_size").click(function() {
     if (points >= size_cost) {
         click_size += .05;
@@ -128,6 +238,7 @@ $("#pic_speed").click(function() {
 });
 // }}
 
+*/
 $("#study20").click(function() {
     $(".flashcard").css('display', 'flex');
     $.ajax({url: "/ajax/question_hw/", success: function(result){
@@ -152,18 +263,6 @@ $(".cardFinished").click(function() {
 }); 
 
 updateSpeed = setInterval(update, 10);
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
